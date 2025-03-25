72194e040b95ca9a7be3a850b0f4e30076f82839
jayjung1226 <54105180+jayjung1226@users.noreply.github.com>
2025-03-25 20:08:06 +0900

    Update analyze.js

--- api/analyze.js
+++ api/analyze.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
       "Authorization": `Bearer ${apiKey}`
     },
     body: JSON.stringify({
-      model: "ft:gpt-3.5-turbo-0125:beflex-inc:tax-v2:BEvcOeC8",
+      model: "ft:gpt-3.5-turbo-0125:beflex-inc:v3:BEwPCAJq",
       messages: req.body.messages
     })
   });
